2baff896e258247b73d32609ab82f315f5bba795
roomem <52628640+roomem@users.noreply.github.com>
2023-11-23 00:35:31 +0100

    Update aks-cluster.tf
    commented resource group creation due to missing user priviledges

--- terraform/aks-cluster.tf
+++ terraform/aks-cluster.tf
@@ -5,7 +5,7 @@ provider "azurerm" {
   features {}
 }
 
-resource "azurerm_resource_group" "BU-MT" {
+/*resource "azurerm_resource_group" "default" {
   name     = "BU-MT"
   location = "westeurope"
 
@@ -13,7 +13,7 @@ resource "azurerm_resource_group" "BU-MT" {
     environment = "Demo"
   }
 }
-
+*/
 resource "azurerm_kubernetes_cluster" "default" {
   name                = "${random_pet.prefix.id}-aks"
   location            = azurerm_resource_group.default.location
