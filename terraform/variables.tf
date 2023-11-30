7a47be39951ded2497ee5399795105b5200dd0f8
Rome <romegioli.work@gmail.com>
2023-11-30 15:15:39 +0100

    variable.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,9 @@
 variable "appId" {
+  default = "2685d5c7-d3dc-4393-bc3c-2fc56fc19067"
   description = "Azure Kubernetes Service Cluster service principal"
 }
 
 variable "password" {
+  default = "Rwq8Q~dvNS6Ve5IDJfTc44z0Y0QbPl0L3W~tDdjn"
   description = "Azure Kubernetes Service Cluster password"
 }
