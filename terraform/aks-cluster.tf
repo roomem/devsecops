042273b63f4fff9b094a32e7afa3c31521530194
Rome <romegioli.work@gmail.com>
2023-11-30 15:09:50 +0100

    clientsecret

--- terraform/aks-cluster.tf
+++ terraform/aks-cluster.tf
@@ -2,9 +2,10 @@
 
 provider "azurerm" {
   features {
-      appId = '2685d5c7-d3dc-4393-bc3c-2fc56fc19067'
-      password = 'Rwq8Q~dvNS6Ve5IDJfTc44z0Y0QbPl0L3W~tDdjn'
   }
+
+  client_id = "2685d5c7-d3dc-4393-bc3c-2fc56fc19067"
+  client_secret = "Rwq8Q~dvNS6Ve5IDJfTc44z0Y0QbPl0L3W~tDdjn"
   skip_provider_registration = true
 }
 
